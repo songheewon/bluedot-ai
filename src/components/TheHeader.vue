ed2e403865db3540e0806224f9816d8035a24c28
Songhee Won <songheew1020@gmail.com>
2025-03-06 17:54:12 +0900

    TheHeader.vue 업데이트

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,6 +1,6 @@
 <template>
   <header>
-    <div class="bg-[#0A142F] w-full h-[60px] flex items-center justify-start md:px-[15%] px-[3%]">
+    <div class="bg-[#0A142F] w-full h-[60px] flex items-center justify-start md:px-[12%] px-[3%]">
       <img src="@/assets/img/logo.svg" alt="bluedot-logo" class="w-[222px] h-[35px]">
     </div>
   </header>
